1445e422017b59c223c1dcf9a06f26c57e43cd72
PramAhmad <pramuditaadit2810@gmail.com>
2026-01-28 07:33:40 -0500

    chore: update selected column  dont use * for select

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ if(isset($_POST['submit'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM users WHERE email='$email'";
+    $sql = "SELECT id,name,email,password,role FROM users WHERE email='$email'";
     $result = mysqli_query($conn, $sql);
     if(mysqli_num_rows($result) > 0) {
         $user = mysqli_fetch_assoc($result);
