20f6b2a7bc54afbc25243096e7e047e5e826a522
PramAhmad <pramuditaadit2810@gmail.com>
2026-01-28 07:52:39 -0500

    refactor(db):remove unused comment

--- db.php
+++ db.php
@@ -1,8 +1,6 @@
 <?php
-// koneksi ke database
 $conn = mysqli_connect("localhost", "root", "", "backendpeminjaman");
 
-// cek koneksi
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
